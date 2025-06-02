469c9cf0fb34b8f64c429043dfd6b53c04c7753f
squirke1 <1stephenquirke@gmail.com>
2025-06-02 18:59:40 +0100

    Implmenting GCP vertexai

--- gcp_vertexai_demo.py
+++ gcp_vertexai_demo.py
@@ -1,15 +1,52 @@
 # gcp_vertexai_demo.py
 
-from vertexai.language_models import ChatModel
-import vertexai
-import time
+from google import genai
+from google.genai import types
+import base64
 
-vertexai.init(project="<your-project-id>", location="us-central1")
-chat_model = ChatModel.from_pretrained("chat-bison")
-chat = chat_model.start_chat()
+def generate():
+  client = genai.Client(
+      vertexai=True,
+      project="smart-impact-459512-f3",
+      location="global",
+  )
 
-start = time.time()
-response = chat.send_message("Explain Retrieval-Augmented Generation (RAG).")
-end = time.time()
 
-print(f"GCP Chat Bison Response (Latency: {round(end - start, 2)}s):\n", response.text)
+  model = "gemini-2.5-flash-preview-05-20"
+  contents = [
+    types.Content(
+      role="user",
+      parts=[
+        types.Part.from_text(text="""I want to compare hyperscalers to see which is the best option for enterprise?""")
+      ]
+    ),
+  ]
+
+  generate_content_config = types.GenerateContentConfig(
+    temperature = 1,
+    top_p = 1,
+    seed = 0,
+    max_output_tokens = 65535,
+    safety_settings = [types.SafetySetting(
+      category=types.HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+      threshold=types.HarmBlockThreshold.BLOCK_NONE
+    ),types.SafetySetting(
+      category=types.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+      threshold=types.HarmBlockThreshold.BLOCK_NONE
+    ),types.SafetySetting(
+      category=types.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+      threshold=types.HarmBlockThreshold.BLOCK_NONE
+    ),types.SafetySetting(
+      category=types.HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold=types.HarmBlockThreshold.BLOCK_NONE
+    )],
+  )
+
+  for chunk in client.models.generate_content_stream(
+    model = model,
+    contents = contents,
+    config = generate_content_config,
+    ):
+    print(chunk.text, end="")
+
+generate()
